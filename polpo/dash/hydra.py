6fe72715df65430db6a8d75a47c74a50a366f64e
Luís F. Pereira <luisfgper@gmail.com>
2024-12-04 16:08:31 -0800

    Move hydra functions to more meaningful place

--- polpo/dash/hydra.py
+++ polpo/dash/hydra.py
@@ -1,7 +1,6 @@
 from polpo.hydra import (
     instantiate_dict_from_config,
     key_value_instantiate,
-    operate_after_instantiate,
 )
 
 
@@ -12,27 +11,5 @@ def load_variables(variables_cfg, name=None):
         name=name,
         instantiate_func=lambda key, value: key_value_instantiate(
             key, value, key_name="id_"
-        ),
-    )
-
-
-def load_data(data_cfg, name=None):
-    # syntax sugar
-    return instantiate_dict_from_config(
-        data_cfg,
-        name=name,
-        instantiate_func=lambda key, value: operate_after_instantiate(
-            value, operation="load"
-        ),
-    )
-
-
-def load_models(model_cfg, name=None):
-    # syntax sugar
-    return instantiate_dict_from_config(
-        model_cfg,
-        name=name,
-        instantiate_func=lambda key, value: operate_after_instantiate(
-            value, operation="create"
         ),
     )
--- polpo/hydra.py
+++ polpo/hydra.py
@@ -41,3 +41,25 @@ def instantiate_dict_from_config(cfg, name=None, instantiate_func=None):
     dict_.update(other)
 
     return dict_
+
+
+def load_data(data_cfg, name=None):
+    # syntax sugar
+    return instantiate_dict_from_config(
+        data_cfg,
+        name=name,
+        instantiate_func=lambda key, value: operate_after_instantiate(
+            value, operation="load"
+        ),
+    )
+
+
+def load_models(model_cfg, name=None):
+    # syntax sugar
+    return instantiate_dict_from_config(
+        model_cfg,
+        name=name,
+        instantiate_func=lambda key, value: operate_after_instantiate(
+            value, operation="create"
+        ),
+    )
