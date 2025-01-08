021dfb681729615d402e5df82b22d9509590855c
Luís F. Pereira <luisfgper@gmail.com>
2025-01-07 21:06:02 -0800

    Update FastSimplificationDecimator to handle absence of colors

--- polpo/preprocessing/mesh/_fast_simplification.py
+++ polpo/preprocessing/mesh/_fast_simplification.py
@@ -6,20 +6,27 @@ from polpo.preprocessing.base import PreprocessingStep
 
 
 class FastSimplificationDecimator(PreprocessingStep):
-    def __init__(self, target_reduction=0.25):
+    def __init__(self, target_reduction=0.25, keep_colors=True):
         super().__init__()
         self.target_reduction = target_reduction
+        self.keep_colors = keep_colors
+
         self._nbrs = NearestNeighbors(n_neighbors=1)
 
     def apply(self, mesh):
-        # TODO: make a check for colors?
-        vertices, faces, colors = mesh
+        if len(mesh) == 3:
+            vertices, faces, colors = mesh
+        else:
+            vertices, faces = mesh
+            colors = None
 
         vertices_, faces_ = fast_simplification.simplify(
             vertices, faces, self.target_reduction
         )
 
-        # TODO: can this be done better?
+        if not self.keep_colors or colors is None:
+            return vertices_, faces_
+
         self._nbrs.fit(vertices)
         _, indices = self._nbrs.kneighbors(vertices_)
         indices = np.squeeze(indices)
