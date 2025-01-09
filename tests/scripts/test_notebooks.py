4fbb1c1b6f1efe28707438c6046deab1256d0310
Luís F. Pereira <luisfgper@gmail.com>
2025-01-08 21:54:34 -0800

    Add notebooks tests

---
+++ tests/scripts/test_notebooks.py
@@ -0,0 +1,17 @@
+"""Unit tests for the notebooks."""
+
+import glob
+
+from geomstats.test.test_case import TestCase
+
+from polpo.testing import NotebooksParametrizer
+
+
+class NotebooksTestData:
+    def __init__(self):
+        NOTEBOOKS_DIR = "notebooks"
+        self.paths = sorted(glob.glob(f"{NOTEBOOKS_DIR}/**/*.ipynb", recursive=True))
+
+
+class TestNotebooks(TestCase, metaclass=NotebooksParametrizer):
+    testing_data = NotebooksTestData()
