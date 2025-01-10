725f512bb4798bfc9115db753e05acdd5bedaa0e
Luís F. Pereira <luisfgper@gmail.com>
2025-01-09 20:19:01 -0800

    Improve mri2mesh pipeline and add corresponding notebook

--- polpo/preprocessing/mri.py
+++ polpo/preprocessing/mri.py
@@ -2,6 +2,8 @@ import nibabel as nib
 import numpy as np
 import skimage
 
+from polpo.utils import params_to_kwargs
+
 from .base import PreprocessingStep
 
 
@@ -31,43 +33,15 @@ class MeshExtractorFromSegmentedImage(PreprocessingStep):
     4, 5 are expected to shrink in volume with progesterone
     """
 
-    def __init__(self, structure_id):
+    def __init__(self, structure_id=-1, marching_cubes=None):
+        if marching_cubes is None:
+            marching_cubes = SkimageMarchingCubes(level=0, method="lewiner")
+
         super().__init__()
         self.structure_id = structure_id
+        self.marching_cubes = marching_cubes
 
-    def apply(self, img_fdata):
-        """Extract one surface mesh from the fdata of a segmented image.
-
-        Parameters
-        ----------
-        img_fdata: array-like, shape = [n_x, n_y, n_z]. Voxels which are colored
-            according to substructure assignment. For example, color of voxel
-            (0, 0, 0) is an integer value that can be anywhere from 0-9.
-        """
-        # print(f"Img fdata shape: {img_fdata.shape}")
-        if self.structure_id == -1:
-            img_mask = img_fdata != 0
-        else:
-            img_mask = img_fdata == self.structure_id
-
-        masked_img_fdata = np.where(img_mask, img_fdata, 0)
-        # print(f"Masked img fdata shape: {masked_img_fdata.shape}")
-        (
-            vertices,
-            faces,
-            _,
-            values,
-        ) = skimage.measure.marching_cubes(  # omitted value is "normals"
-            masked_img_fdata,
-            level=0,
-            step_size=1,
-            allow_degenerate=False,
-            method="lorensen",
-        )
-        # print(f"Colors: {values.min()}, {values.max()}")
-        # print(f"Vertices.shape = {vertices.shape}")
-
-        color_dict = {  # trimesh uses format [r, g, b, a] where a is alpha
+        self.color_dict = {  # trimesh uses format [r, g, b, a] where a is alpha
             1: [255, 0, 0, 255],
             2: [0, 255, 0, 255],
             3: [0, 0, 255, 255],
@@ -79,8 +53,63 @@ class MeshExtractorFromSegmentedImage(PreprocessingStep):
             9: [184, 115, 51, 255],
         }
 
-        colors = np.array([np.array(color_dict[value]) for value in values])
-        # print(f"Colors.shape = {colors.shape}")
-        # print("Colors:", colors)
+    def apply(self, img_fdata):
+        """Extract one surface mesh from the fdata of a segmented image.
 
+        Parameters
+        ----------
+        img_fdata: array-like, shape = [n_x, n_y, n_z]. Voxels which are colored
+            according to substructure assignment. For example, color of voxel
+            (0, 0, 0) is an integer value that can be anywhere from 0-9.
+        """
+        # TODO: implement as a pipeline (syntax sugar!)
+
+        if self.structure_id == -1:
+            img_mask = img_fdata != 0
+        else:
+            img_mask = img_fdata == self.structure_id
+
+        masked_img_fdata = np.where(img_mask, img_fdata, 0)
+        (
+            vertices,
+            faces,
+            values,
+        ) = self.marching_cubes(masked_img_fdata)
+
+        colors = np.array([np.array(self.color_dict[value]) for value in values])
         return vertices, faces, colors
+
+
+class SkimageMarchingCubes(PreprocessingStep):
+    """
+    https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.marching_cubes
+    """
+
+    def __init__(
+        self,
+        level=0,
+        step_size=1,
+        allow_degenerate=False,
+        method="lewiner",
+    ):
+        self.level = level
+        self.step_size = step_size
+        self.allow_degenerate = allow_degenerate
+        self.method = method
+
+    def apply(self, data):
+        if isinstance(data, tuple):
+            img_fdata, mask = data
+        else:
+            img_fdata = data
+            mask = None
+
+        (
+            vertices,
+            faces,
+            _,
+            values,
+        ) = skimage.measure.marching_cubes(
+            img_fdata, mask=mask, **params_to_kwargs(self)
+        )
+        return vertices, faces, values
