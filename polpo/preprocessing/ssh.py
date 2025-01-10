4f68d3afb1e0d0220b09ab2234232578cc928860
Luís F. Pereira <luisfgper@gmail.com>
2025-01-09 19:41:26 -0800

    Add SshClient encapsulating connection to host and use it in ScpDataLoader

--- polpo/preprocessing/ssh.py
+++ polpo/preprocessing/ssh.py
@@ -8,48 +8,68 @@ from polpo.defaults import DATA_DIR
 from .base import CacheableDataLoader, PreprocessingStep
 
 
-def connect_using_ssh_config(host_name):
-    """Connect to a host using SSH config."""
-    config = paramiko.SSHConfig()
-    with open(os.path.expanduser("~/.ssh/config")) as file:
-        config.parse(file)
+class SshClient:
+    def __init__(self, host_name="frank"):
+        self.host_name = host_name
+        self._client = None
 
-    host_config = config.lookup(host_name)
+    def connect(self):
+        """Connect to a host using SSH config."""
+        config = paramiko.SSHConfig()
+        with open(os.path.expanduser("~/.ssh/config")) as file:
+            config.parse(file)
 
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        host_config = config.lookup(self.host_name)
 
-    client.connect(
-        hostname=host_config.get("hostname", host_name),
-        port=host_config.get("port", 22),
-        username=host_config.get("user"),
-        key_filename=host_config.get("identityfile"),
-    )
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    return client
+        client.connect(
+            hostname=host_config.get("hostname", self.host_name),
+            port=host_config.get("port", 22),
+            username=host_config.get("user"),
+            key_filename=host_config.get("identityfile"),
+        )
+
+        self._client = client
+
+        return self
+
+    def to_scp(self):
+        if self._client is None:
+            self.connect()
+
+        return scp.SCPClient(self._client.get_transport())
 
 
-class SCPDataLoader(PreprocessingStep, CacheableDataLoader):
-    """
+class ScpDataLoader(PreprocessingStep, CacheableDataLoader):
+    """Transfer files and directories from remote host to localhost.
 
     Parameters
     ----------
+    remote_path : str
+        Path to retrieve from remote host.
+    ssh_client : SshClient
+        Client handling connection.
     data_dir : str
         Directory where to store data.
+    recursive : str
+        Whether to transfer files and directories recursively.
+    use_cache : bool
+        Whether to verify if data is already available locally.
+    local_basename : str
+        Basename of transferred file/folder if different from remote host.
     """
 
     def __init__(
         self,
         remote_path,
+        ssh_client,
         data_dir=None,
         recursive=True,
         use_cache=True,
-        ssh_client=None,
-        host_name=None,
+        local_basename=None,
     ):
-        # host_name is ignored if ssh_client
-        # host_name is lazy
-        # TODO: recursive to None and check if has extension?
         super().__init__(use_cache)
 
         if data_dir is None:
@@ -58,10 +78,11 @@ class SCPDataLoader(PreprocessingStep, CacheableDataLoader):
         if "~" in data_dir:
             data_dir = os.path.expanduser(data_dir)
 
-        self._ssh_client = ssh_client if ssh_client is not None else host_name
+        self.ssh_client = ssh_client
         self.remote_path = remote_path
         self.recursive = recursive
         self.data_dir = data_dir
+        self.local_basename = local_basename
 
     @property
     def _path(self):
@@ -69,16 +90,27 @@ class SCPDataLoader(PreprocessingStep, CacheableDataLoader):
         return os.path.join(self.data_dir, remote_path_ls[-1])
 
     @property
-    def ssh_client(self):
-        if isinstance(self._ssh_client, str):
-            self._ssh_client = connect_using_ssh_config(self._ssh_client)
-        return self._ssh_client
+    def _renamed_path(self):
+        if self.local_basename is None:
+            return self._path
+
+        return os.path.join(self.data_dir, self.local_basename)
 
     @classmethod
-    def from_ssh_host(
-        cls, host_name, remote_path, data_dir=None, recursive=True, use_cache=True
+    def from_host_name(
+        cls,
+        host_name,
+        remote_path,
+        data_dir=None,
+        recursive=True,
+        use_cache=True,
+        local_basename=None,
+        force_connect=False,
     ):
-        ssh_client = connect_using_ssh_config(host_name)
+        ssh_client = SshClient(host_name)
+        # allows to detect inability to connect at instantiation
+        if force_connect:
+            ssh_client.connect()
 
         return cls(
             remote_path,
@@ -86,21 +118,25 @@ class SCPDataLoader(PreprocessingStep, CacheableDataLoader):
             data_dir=data_dir,
             recursive=recursive,
             use_cache=use_cache,
+            local_basename=local_basename,
         )
 
     def load(self):
-        path = self._path
+        path = self._renamed_path
 
         if self.exists(path):
             return path
 
         os.makedirs(self.data_dir, exist_ok=True)
-        with scp.SCPClient(self.ssh_client.get_transport()) as scp_client:
+        with self.ssh_client.to_scp() as scp_client:
             scp_client.get(
                 remote_path=self.remote_path,
                 local_path=self.data_dir,
                 recursive=self.recursive,
             )
+
+        if self.local_basename and self._path != self._renamed_path:
+            os.rename(self._path, self._renamed_path)
 
         return path
 
