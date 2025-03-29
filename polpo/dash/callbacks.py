6f3a717487710717028baef49947bf378cb3c723
Luís F. Pereira <luisfgper@gmail.com>
2025-03-28 22:40:07 -0700

    Generalize callback for model view update with toggler and checklist

--- polpo/dash/callbacks.py
+++ polpo/dash/callbacks.py
@@ -1,4 +1,4 @@
-from dash import Input, callback
+from dash import Input, callback, ctx
 
 from polpo.utils import unnest_list
 
@@ -20,6 +20,7 @@ def create_view_model_update(
         pred = model.predict(args)
         return output_view.to_dash(pred)
 
+
 def create_button_toggler(toggle_id, hideable_components):
     n_components = len(hideable_components)
 
@@ -40,48 +41,24 @@ def create_button_toggler(toggle_id, hideable_components):
 
         return out
 
-# def create_button_toggler_for_view_model_update(
-#     input_views, output_view, models, toggle_id, hideable_components
-# ):
-#     # NB: a simple merge of the two above to avoid chained callbacks
-#     empty_output = output_view.as_empty_output()
 
-#     n_components = len(hideable_components)
-
-#     inputs = []
-#     indices = [0]
-#     for input_view in input_views:
-#         input_ = input_view.as_input()
-#         indices.append(indices[-1] + len(input_))
-#         inputs.extend(input_)
-
-#     @callback(
-#         unnest_list(
-#             [
-#                 component.as_output(component_property="style")
-#                 for component in hideable_components
-#             ]
-#         )
-#         + output_view.as_output(),
-#         *([Input(toggle_id, "n_clicks")] + inputs),
-#     )
-#     def button_toggler(n_clicks, *args):
-#         out = [{"display": "none"}] * n_components
-
-#         if args[0] is None:
-#             return out + empty_output
-
-#         show_index = n_clicks % n_components
-#         model = models[show_index]
-#         model_args = args[indices[show_index] : indices[show_index + 1]]
-
-#         out[show_index] = {"display": "block"}
-#         pred = model.predict(model_args)
-
-#         return out + output_view.to_dash(pred)
 def create_button_toggler_for_view_model_update(
-    input_views, output_view, models, toggle_id, checkbox_id, hideable_components
+    input_views, output_view, models, toggle_id, checklist, hideable_components
 ):
+    """
+
+    Parameters
+    ----------
+    input_views : list[Component]
+    output_view : Component
+    models : list[Model]
+        One model per input view.
+    checklist : Checklist or DummyComponent
+    hideable_components : list[HideableComponent]
+    models : list[Model]
+    """
+    # TODO: connect input view with hideable component?
+
     # NB: a simple merge of the two above to avoid chained callbacks
     empty_output = output_view.as_empty_output()
 
@@ -102,9 +79,15 @@ def create_button_toggler_for_view_model_update(
             ]
         )
         + output_view.as_output(),
-        *([Input(toggle_id, "n_clicks"),Input(checkbox_id, "value"),] + inputs),
+        *(
+            [
+                Input(toggle_id, "n_clicks"),
+            ]
+            + inputs
+            + checklist.as_input()
+        ),
     )
-    def button_toggler(n_clicks, checkbox_value, *args):
+    def button_toggler(n_clicks, *args):
         out = [{"display": "none"}] * n_components
 
         if args[0] is None:
@@ -115,8 +98,11 @@ def create_button_toggler_for_view_model_update(
         model_args = args[indices[show_index] : indices[show_index + 1]]
 
         out[show_index] = {"display": "block"}
-        pred = model.predict(model_args)
 
-        show_template = "show" in checkbox_value
+        # TODO: create collection of models
+        if ctx.triggered_id == checklist.id:
+            pred = checklist.as_bool(args[-1])
+        else:
+            pred = model.predict(model_args)
 
-        return out + output_view.to_dash(pred, show_template=show_template)
+        return out + output_view.to_dash(pred)
