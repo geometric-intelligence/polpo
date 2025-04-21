c93a2c5eb972431a7a6bf799dc09a83e6949128d
Luís F. Pereira <luisfgper@gmail.com>
2025-04-21 13:47:01 -0700

    Add more options for mesh explorer dev app

--- polpo/__main__.py
+++ polpo/__main__.py
@@ -3,12 +3,21 @@ import typer
 app = typer.Typer()
 
 
+from enum import Enum
+
+
+class DataOptions(str, Enum):
+    hipp = "hipp"
+    maternal = "maternal"
+    multiple = "multiple"
+
+
 @app.command()
-def mesh_explorer():
+def mesh_explorer(data: DataOptions = DataOptions.hipp):
     """Launch mesh_explorer app."""
     from polpo.dash.app.mesh_explorer import my_app
 
-    my_app()
+    my_app(data=data.value)
 
 
 if __name__ == "__main__":
--- polpo/dash/app/mesh_explorer.py
+++ polpo/dash/app/mesh_explorer.py
@@ -1,16 +1,20 @@
 import dash_bootstrap_components as dbc
+import numpy as np
 from dash import Dash
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
 
-from polpo.dash.components import MeshExplorer, Slider
+from polpo.dash.components import Graph, MeshExplorer, Slider
 from polpo.dash.style import update_style
 from polpo.dash.variables import VarDef
-from polpo.models import Meshes2Comps, ObjectRegressor
+from polpo.models import DictMeshes2Comps, Meshes2Comps, ObjectRegressor
+from polpo.plot.mesh import MeshesPlotter, MeshPlotter
 from polpo.preprocessing import (
     IndexMap,
+    ListSqueeze,
     Map,
     NestingSwapper,
+    PartiallyInitializedStep,
     Pipeline,
     WrapInList,
 )
@@ -20,19 +24,25 @@ from polpo.preprocessing import (
 from polpo.preprocessing import pd as ppd
 from polpo.preprocessing.load.pregnancy import (
     DenseMaternalCsvDataLoader,
+    DenseMaternalMeshLoader,
     PregnancyPilotRegisteredMeshesLoader,
 )
-from polpo.preprocessing.mesh.io import TrimeshReader
+from polpo.preprocessing.mesh.conversion import TrimeshFromPv
+from polpo.preprocessing.mesh.io import PvReader, TrimeshReader
+from polpo.preprocessing.mesh.registration import PvAlign
+from polpo.preprocessing.mri import segmtool2encoding
 
 
-def _load_session_week():
-    hormones_df = Pipeline(
+def _load_homornes_df():
+    return Pipeline(
         steps=[
             DenseMaternalCsvDataLoader(pilot=True),
             ppd.Drop(labels=27),
         ]
     )()
 
+
+def _load_session_week(hormones_df):
     session_week_dict = Pipeline(
         steps=[
             ppd.ColumnsSelector(column_names="gestWeek"),
@@ -52,12 +62,56 @@ def _load_hipp_meshes():
     )()
 
 
-def _load_data():
-    return _load_session_week(), _load_hipp_meshes()
+def _load_maternal_pilot():
+    return Pipeline(
+        steps=[
+            DenseMaternalMeshLoader(subject_id=None, as_dict=True),
+            ppdict.DictMap(step=PvReader()),
+            PartiallyInitializedStep(
+                Step=lambda target, max_iterations: ppdict.DictMap(
+                    step=PvAlign(target=target, max_iterations=max_iterations)
+                ),
+                _target=lambda meshes: meshes[1],  # template mesh
+                max_iterations=500,
+            ),
+            ppdict.DictMap(step=TrimeshFromPv()),
+        ]
+    )()
+
+
+def _load_maternal_multiple(subject_id="01", tool="fsl"):
+    encoding = segmtool2encoding(tool)
+
+    struct_keys = encoding.structs
+
+    mesh_loader = ppdict.HashWithIncoming(
+        Map(
+            PartiallyInitializedStep(
+                Step=DenseMaternalMeshLoader,
+                pass_data=False,
+                subject_id=subject_id,
+                _struct=lambda name: name.split("_")[-1],
+                _left=lambda name: name.split("_")[0] == "L",
+                as_dict=True,
+            )
+            + ppdict.DictMap(PvReader())
+        )
+    )
+
+    prep_pipe = PartiallyInitializedStep(
+        Step=lambda **kwargs: ppdict.DictMap(PvAlign(**kwargs) + TrimeshFromPv()),
+        _target=lambda meshes: meshes[list(meshes.keys())[0]],
+        max_iterations=500,
+    )
+
+    pipe = mesh_loader + ppdict.DictMap(prep_pipe)
+
+    meshes = pipe(struct_keys)
+
+    return meshes
 
 
 def _merge_session_week_meshes(session_week, registered_meshes):
-    # data: hormones, registered_meshes
     pipeline = Pipeline(
         steps=[
             ppdict.DictMerger(),
@@ -66,9 +120,9 @@ def _merge_session_week_meshes(session_week, registered_meshes):
         ]
     )
 
-    X, y = pipeline((session_week, registered_meshes))
+    X, meshes = pipeline((session_week, registered_meshes))
 
-    return X, y
+    return X, meshes
 
 
 def _instantiate_week_mesh_model(session_week, registered_meshes):
@@ -87,6 +141,34 @@ def _instantiate_week_mesh_model(session_week, registered_meshes):
     return model
 
 
+def _merge_session_week_multi_meshes(session_week, registered_meshes):
+    dict_pipe = (
+        IndexMap(ppdict.NestedDictSwapper(), index=1)
+        + ppdict.DictMerger()
+        + NestingSwapper()
+        + IndexMap(lambda x: np.array(x)[:, None], index=0)
+        + IndexMap(ppdict.ListDictSwapper(), index=1)
+    )
+
+    # meshes_ : dict[list]
+    X, meshes_ = dict_pipe([session_week, registered_meshes])
+
+    return X, meshes_
+
+
+def _instantiate_week_multi_mesh_model(session_week, registered_meshes):
+    X, y = _merge_session_week_multi_meshes(session_week, registered_meshes)
+    n_structs = len(y)
+
+    pca = PCA(n_components=4)
+    objs2y = DictMeshes2Comps(n_pipes=n_structs, dim_reduction=pca)
+
+    model = ObjectRegressor(LinearRegression(fit_intercept=True), objs2y=objs2y)
+    model.fit(X, y)
+
+    return model
+
+
 def _create_week_inputs():
     gest_week = VarDef(
         id_="GestWeek",
@@ -99,18 +181,47 @@ def _create_week_inputs():
     return Slider(gest_week)
 
 
-def _hipp_week_single():
-    session_week, registered_meshes = _load_data()
-    model = _instantiate_week_mesh_model(session_week, registered_meshes)
+Key2MeshLoader = {
+    "hipp": _load_hipp_meshes,
+    "maternal": _load_maternal_pilot,
+    "multiple": _load_maternal_multiple,
+}
+
+Key2ModelInstantiator = {
+    "hipp": _instantiate_week_mesh_model,
+    "maternal": _instantiate_week_mesh_model,
+    "multiple": _instantiate_week_multi_mesh_model,
+}
+
+
+def _week_mesh_layout(data="hipp"):
+    hormones_df = _load_homornes_df()
+    session_week = _load_session_week(hormones_df)
 
     inputs = _create_week_inputs()
 
-    mesh_explorer = MeshExplorer(model=model, inputs=inputs)
+    registered_meshes = Key2MeshLoader[data]()
+    model = Key2ModelInstantiator[data](session_week, registered_meshes)
+
+    postproc_pred = None
+    graph = None
+    if data == "multiple":
+        plotter = MeshesPlotter([MeshPlotter() for _ in range(len(registered_meshes))])
+        graph = Graph(id_="mesh-plot", plotter=plotter)
+
+        postproc_pred = ppdict.DictMap(ListSqueeze()) + ppdict.DictToValuesList()
+
+    mesh_explorer = MeshExplorer(
+        model=model,
+        inputs=inputs,
+        graph=graph,
+        postproc_pred=postproc_pred,
+    )
 
     return dbc.Container(mesh_explorer.to_dash())
 
 
-def my_app():
+def my_app(data="hipp"):
     style = {
         "margin_side": "20px",
         "text_fontsize": "24px",
@@ -121,7 +232,7 @@ def my_app():
     }
     update_style(style)
 
-    layout = _hipp_week_single()
+    layout = _week_mesh_layout(data)
 
     app = Dash(
         __name__,
