af3be2614fc794cbe6a52d7eca7570ddd8d9366b
Luís F. Pereira <luisfgper@gmail.com>
2025-05-02 14:46:24 -0700

    Add DataBasedParametrizer (to be updated in geomstats)

--- polpo/testing.py
+++ polpo/testing.py
@@ -2,7 +2,13 @@ import json
 import os
 
 import pytest
-from geomstats.test.parametrizers import _exec_notebook, _raise_missing_testing_data
+from geomstats.test.parametrizers import (
+    _activate_tests_given_data,
+    _collect_all_tests,
+    _exec_notebook,
+    _raise_missing_testing_data,
+    _update_attrs,
+)
 
 
 class NotebooksParametrizer(type):
@@ -34,3 +40,46 @@ class NotebooksParametrizer(type):
             attrs[func_name] = test_func
 
         return super().__new__(cls, name, bases, attrs)
+
+
+class DataBasedParametrizer(type):
+    """Metaclass for test classes driven by data definition.
+
+    It differs from `Parametrizer` because every test data function must have
+    an associated test function, instead of the opposite.
+    """
+
+    # TODO: bring to geomstats
+
+    def __new__(cls, name, bases, attrs):
+        testing_data = locals()["attrs"].get("testing_data")
+        _raise_missing_testing_data(testing_data)
+
+        test_funcs = _collect_all_tests(attrs, bases, active=False)
+
+        _activate_tests_given_data(test_funcs, testing_data)
+        if testing_data.skip_all:
+            for test_func in test_funcs.values():
+                test_func.add_mark("skip")
+
+        _update_attrs(test_funcs, testing_data, attrs)
+
+        # TODO: improve logic
+        testing_data.deactivate = testing_data.skip_all
+        if testing_data.deactivate:
+            return super().__new__(cls, name, bases, attrs)
+
+        for func in test_funcs.values():
+            if func.active and not func.skip:
+                break
+        else:
+            testing_data.deactivate = True
+
+        return super().__new__(cls, name, bases, attrs)
+
+
+@pytest.fixture(scope="class")
+def data_check(request):
+    testing_data = request.cls.testing_data
+    if testing_data.deactivate:
+        pytest.skip()
