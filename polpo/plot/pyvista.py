6ecf128fdb68687acac068715b92cb9691a88a0b
Luís F. Pereira <luisfgper@gmail.com>
2025-09-03 17:26:32 -0700

    Add RegisteredMeshesGifPlotter

---
+++ polpo/plot/pyvista.py
@@ -0,0 +1,90 @@
+import pyvista as pv
+
+import polpo.utils as putils
+
+
+class RegisteredMeshesGifPlotter:
+    def __init__(
+        self,
+        gif_name,
+        shape=(1, 1),
+        fps=10,
+        border=False,
+        off_screen=True,
+        notebook=False,
+        subtitle=None,
+        **kwargs,
+    ):
+        if subtitle is True:
+            subtitle = lambda x, y: str(y)
+
+        self.fps = fps
+        self.gif_name = gif_name
+        self.subtitle = subtitle
+
+        self.pl = pv.Plotter(
+            shape=shape,
+            border=border,
+            off_screen=off_screen,
+            notebook=notebook,
+            **kwargs,
+        )
+
+        self._gif = None
+
+        self._subtitle_kwargs = {"font_size": 8}
+
+    def set_subtitle_kwargs(self, kwargs):
+        self._subtitle_kwargs.update(kwargs)
+
+        return self
+
+    def _iter_inner(self, meshes):
+        if isinstance(meshes, dict):
+            return meshes.items()
+
+        return enumerate(meshes)
+
+    def add_meshes(self, meshes, show_edges=True, **kwargs):
+        """Add meshes to gif.
+
+        Parameters
+        ----------
+        meshes : list[pv.Mesh] or list[list[pv.Mesh]]
+            Axes: time, subplot.
+        """
+        pl = self.pl
+
+        if self._gif is None:
+            self._gif = pl.open_gif(self.gif_name, fps=self.fps)
+
+        if pl.shape[0] * pl.shape[1] == 1:
+            meshes = [[mesh] for mesh in meshes]
+
+        rendered_meshes = {}
+        for time_index, meshes_ in enumerate(meshes):
+            for subplot_index, (comp_id, mesh) in enumerate(self._iter_inner(meshes_)):
+                pl.subplot(*putils.plot_index_to_shape(subplot_index, pl.shape[1]))
+
+                if time_index:
+                    rendered_meshes[subplot_index].points = mesh.points
+                else:
+                    rendered_meshes[subplot_index] = mesh_ = mesh.copy()
+                    pl.add_mesh(mesh_, show_edges=show_edges, **kwargs)
+
+                    if callable(self.subtitle):
+                        pl.add_title(
+                            self.subtitle(time_index, comp_id),
+                            **self._subtitle_kwargs,
+                        )
+
+            pl.write_frame()
+
+    def close(self):
+        self.pl.close()
+
+    def show(self):
+        # show in notebook
+        from IPython.display import Image
+
+        return Image(open(self.gif_name, "rb").read())
