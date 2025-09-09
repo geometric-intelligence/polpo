1e83b59c4e0166c13479765de41ecbf6693aefa6
Luís F. Pereira <luisfgper@gmail.com>
2025-09-09 16:31:20 -0700

    Add RigidAlignment

--- polpo/preprocessing/mesh/registration.py
+++ polpo/preprocessing/mesh/registration.py
@@ -1,3 +1,5 @@
+import polpo.preprocessing.dict as ppdict
+from polpo.preprocessing._preprocessing import Map, PartiallyInitializedStep
 from polpo.preprocessing.base import PreprocessingStep
 
 try:
@@ -22,3 +24,21 @@ class IdentityMeshAligner(PreprocessingStep):
     def __call__(self, meshes):
         target_mesh, _ = meshes
         return target_mesh
+
+
+class RigidAlignment(PartiallyInitializedStep):
+    def __init__(self, target=None, is_dict=True, **kwargs):
+        # TODO: create a more robust iterator/template selector?
+
+        if target is None:
+            if is_dict:
+                target = lambda x: next(iter(x.values()))
+            else:
+                target = lambda x: x[0]
+
+        if is_dict:
+            Step = lambda **_kwargs: ppdict.DictMap(PvAlign(**_kwargs))
+        else:
+            Step = lambda **_kwargs: Map(PvAlign(**_kwargs))
+
+        super().__init__(Step=Step, _target=target, **kwargs)
