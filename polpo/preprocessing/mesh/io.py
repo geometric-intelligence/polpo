4edae68c1c27e6f4e40baf4828b01dfdaa7106e8
Luís F. Pereira <luisfgper@gmail.com>
2025-09-10 17:17:49 -0700

    Add convenient DictMeshWriter

--- polpo/preprocessing/mesh/io.py
+++ polpo/preprocessing/mesh/io.py
@@ -17,3 +17,25 @@ try:
     from ._nibabel import FreeSurferReader  # noqa:F401
 except ImportError:
     pass
+
+from polpo.preprocessing.base import PreprocessingStep
+
+
+class DictMeshWriter(PreprocessingStep):
+    def __init__(self, mesh_writer=None, key2name=None, **kwargs):
+        super().__init__()
+
+        if mesh_writer is None:
+            mesh_writer = PvWriter(**kwargs)
+
+        if key2name is None:
+            key2name = lambda key: f"mesh_{key}"
+
+        self.mesh_writer = mesh_writer
+        self.key2name = key2name
+
+    def __call__(self, data):
+        return {
+            key: self.mesh_writer((self.key2name(key), mesh))
+            for key, mesh in data.items()
+        }
