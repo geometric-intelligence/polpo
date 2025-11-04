26bbefc9e97dbb101c1469bd08229a92da67fe26
Luís F. Pereira <luisfgper@gmail.com>
2025-11-04 10:53:09 -0800

    Add sorting to subject ids loading

--- polpo/preprocessing/load/pregnancy/jacobs.py
+++ polpo/preprocessing/load/pregnancy/jacobs.py
@@ -39,7 +39,7 @@ from .pilot import TabularDataLoader as PilotTabularDataLoader
 MATERNAL_IDS = {"01", "1001B", "1004B", "2004B", "1009B"}
 
 
-def get_subject_ids(include_pilot=True, include_male=True):
+def get_subject_ids(include_pilot=True, include_male=True, sort=False):
     ids = MATERNAL_IDS.copy()
 
     if not include_pilot:
@@ -49,6 +49,9 @@ def get_subject_ids(include_pilot=True, include_male=True):
         for id_ in ids.copy():
             if id_.startswith("2"):
                 ids.remove(id_)
+
+    if sort:
+        ids = sorted(ids)
 
     return ids
 
