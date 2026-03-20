3c81efe74bb2a3a7a502cf89bc01836f7faf997d
Luís F. Pereira <luisfgper@gmail.com>
2026-03-19 23:42:53 -0700

    Add lddmm_to_global for jacobs

---
+++ scripts/lddmm_to_global/jacobs.py
@@ -0,0 +1,104 @@
+import json
+import logging
+from pathlib import Path
+
+import polpo.preprocessing.dict as ppdict
+import polpo.utils as putils
+from polpo.preprocessing.load.fsl import get_all_first_structs
+from polpo.preprocessing.load.pregnancy.jacobs import (
+    MeshLoader,
+    get_key_to_week,
+    get_subject_ids,
+)
+from polpo.protocol.lddmm_to_global import LddmmToGlobal
+
+
+def protocol_per_struct(
+    struct, subject_ids, data_dir, results_dir, atlases_keys, derivative="enigma"
+):
+    # for serialization
+    params = dict(
+        struct=struct,
+        subject_ids=subject_ids,
+        derivative=derivative,
+        data_dir=data_dir,
+    )
+    with open(results_dir / "params_maternal.json", "w") as file:
+        json.dump(params, file, indent=4)
+
+    known_correspondences = True if derivative == "enigma" else False
+
+    dataset = (
+        MeshLoader(
+            data_dir=data_dir,
+            subject_subset=subject_ids,
+            struct_subset=[struct],
+            derivative=derivative,
+            as_mesh=True,
+        )
+        + ppdict.ExtractUniqueKey(nested=True)
+    )()
+
+    protocol = LddmmToGlobal(
+        known_correspondences,
+        results_dir=results_dir,
+    )
+
+    protocol.run(dataset, atlases_keys=atlases_keys)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        filename="lddmm_to_global_jacobs.log",
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        force=True,
+    )
+
+    # structs = get_all_first_structs(order=True, include_brstem=False)
+    structs = ["L_Hipp", "R_Hipp", "L_Puta", "R_Puta"]
+
+    subject_ids = get_subject_ids(sort=True)
+    subject_ids.remove("1009B")  # no pre meshes
+
+    derivative = "enigma"
+
+    data_dir = (
+        "/scratch/data/maternal"
+        if putils.in_frank()
+        else Path.home() / ".herbrain/data/maternal"
+    )
+
+    key2week = get_key_to_week(data_dir=data_dir)
+    key_filter = ppdict.DictFilter(func=(lambda x: x < 0))
+    atlases_keys = {}
+    for subject_id in subject_ids:
+        atlases_keys[subject_id] = pre_keys = list(
+            key_filter(key2week[subject_id]).keys()
+        )
+        if len(pre_keys) < 1:
+            raise ValueError(f"No pre meshes for {subject_id}")
+
+    for struct in structs:
+        results_dir = (
+            putils.get_results_path()
+            / "lddmm_to_global"
+            / "jacobs"
+            / f"{struct}_{derivative}"
+        )
+        if results_dir.exists():
+            logging.info(f"Skipping {struct} because folder already exists")
+            continue
+
+        results_dir.mkdir(parents=True, exist_ok=False)
+
+        try:
+            protocol_per_struct(
+                struct,
+                subject_ids=subject_ids,
+                data_dir=data_dir,
+                results_dir=results_dir,
+                atlases_keys=atlases_keys,
+            )
+        except Exception as e:
+            logging.warning(f"Oops, something went wrong for {struct}: {e}")
