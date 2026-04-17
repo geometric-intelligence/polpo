6f71ab66d19590cb0ce1f8b75967483db2746ed5
Luís F. Pereira <luisfgper@gmail.com>
2026-04-16 18:23:11 -0700

    Add jacobs control subject

--- polpo/jacobs/utils.py
+++ polpo/jacobs/utils.py
@@ -1,16 +1,27 @@
-MATERNAL_IDS = {"01", "1001B", "1004B", "2004B", "1009B"}
+MATERNAL_IDS = {
+    "01",
+    "1001B",
+    "1004B",
+    "1009B",
+    "2004B",
+    "3004B",
+}
 
 
-def get_subject_ids(include_pilot=True, include_male=True, sort=False):
+def get_subject_ids(
+    include_pilot=True, include_male=True, include_control=True, sort=False
+):
     ids = MATERNAL_IDS.copy()
 
     if not include_pilot:
         ids.remove("01")
 
-    if not include_male:
-        for id_ in ids.copy():
-            if id_.startswith("2"):
-                ids.remove(id_)
+    for id_ in ids.copy():
+        if not include_male and id_.startswith("2"):
+            ids.remove(id_)
+
+        if not include_control and id_.startswith("3"):
+            ids.remove(id_)
 
     if sort:
         ids = sorted(ids)
