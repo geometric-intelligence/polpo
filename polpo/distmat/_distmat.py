636ec796a8829634f33a9d8af89f88a1a48a126a
Luís F. Pereira <luisfgper@gmail.com>
2026-08-10 16:12:03 -0700

    Upgrade PairwiseDistances

--- polpo/distmat/_distmat.py
+++ polpo/distmat/_distmat.py
@@ -1,17 +1,22 @@
 import numpy as np
 
+from polpo.dataset import Dataset
 from polpo.numpy.io import load_indexed_array, save_indexed_array
 from polpo.utils.np import triu_vec_to_sym
 
+from .plot import plot_dist_mat
 
-def sort_dist_mat(mat):
+
+def sort_by_row_norm(mat, descending=True):
     # checks for global isolation
-    dist_norms = np.linalg.norm(mat, axis=-1)
-    sorted_idx = np.argsort(-dist_norms, axis=-1)
+    row_norms = np.linalg.norm(mat, axis=-1)
 
-    perm_dists = mat[sorted_idx][:, sorted_idx]
+    signal = -1.0 if descending else 1.0
+    sorted_idx = np.argsort(signal * row_norms, axis=-1)
 
-    return perm_dists, sorted_idx
+    perm_mat = mat[np.ix_(sorted_idx, sorted_idx)]
+
+    return perm_mat, sorted_idx
 
 
 def knn_scores(mat, k=5):
@@ -22,28 +27,45 @@ def knn_scores(mat, k=5):
 
 
 class PairwiseDistances:
-    def __init__(self, keys, data):
-        self.keys = keys
+    def __init__(self, labels, data):
+        self.labels = labels
         self.data = data
 
         self._validate()
 
-        self._key_to_index = {key: index for index, key in enumerate(self.keys)}
+        self._label_to_index = {label: index for index, label in enumerate(self.labels)}
 
     def _validate(self):
-        expected = len(self.keys) * (len(self.keys) - 1) // 2
+        expected = len(self.labels) * (len(self.labels) - 1) // 2
 
         if len(self.data) != expected:
-            raise ValueError(
-                f"Expected {expected} distances, " f"got {len(self.data)}."
-            )
+            raise ValueError(f"Expected {expected} distances, got {len(self.data)}.")
 
     @property
     def matrix(self):
         return triu_vec_to_sym(self.data)
 
-    def index(self, key):
-        return self._key_to_index[key]
+    @classmethod
+    def from_matrix(cls, labels, matrix):
+        indices = np.triu_indices(len(labels), k=1)
+        return cls(labels, matrix[indices])
+
+    @property
+    def pairs(self):
+        return [
+            (self.labels[i], self.labels[j])
+            for i in range(len(self.labels))
+            for j in range(i + 1, len(self.labels))
+        ]
+
+    def items(self):
+        return zip(self.pairs, self.data)
+
+    def as_dataset(self):
+        return Dataset(dict(self.items()))
+
+    def _index(self, label):
+        return self._label_to_index[label]
 
     @staticmethod
     def _pair_index(i, j, n):
@@ -55,20 +77,42 @@ class PairwiseDistances:
 
         return n * i - i * (i + 1) // 2 + (j - i - 1)
 
-    def get(self, key_a, key_b):
-        i = self.index(key_a)
-        j = self.index(key_b)
+    def get(self, label_a, label_b):
+        i = self._index(label_a)
+        j = self._index(label_b)
 
         if i == j:
             return 0.0
 
-        index = self._pair_index(i, j, len(self.keys))
+        index = self._pair_index(i, j, len(self.labels))
         return self.data[index]
 
     def save(self, path):
-        return save_indexed_array(path, self.keys, self.data)
+        return save_indexed_array(path, self.labels, self.data)
 
     @classmethod
     def load(cls, path):
-        keys, data = load_indexed_array(path)
-        return cls(keys=keys, data=data)
+        labels, data = load_indexed_array(path)
+        return cls(labels=labels, data=data)
+
+    def sort_by_row_norm(self, descending=True):
+        perm_mat, sorted_idx = sort_by_row_norm(self.matrix)
+        labels = [self.labels[index] for index in sorted_idx]
+        return self.__class__.from_matrix(labels, perm_mat)
+
+    def plot(self, **kwargs):
+        return plot_dist_mat(self, **kwargs)
+
+    def select(self, labels):
+        data = [self.get(a, b) for i, a in enumerate(labels) for b in labels[i + 1 :]]
+
+        return self.__class__(labels, np.array(data))
+
+    def filter_labels(self, predicate):
+        return self.select([label for label in self.labels if predicate(label)])
+
+    def map_labels(self, func):
+        return self.__class__(
+            labels=[func(label) for label in self.labels],
+            data=self.data,
+        )
--- polpo/distmat/plot.py
+++ polpo/distmat/plot.py
@@ -1,23 +1,18 @@
-import numpy as np
 from matplotlib import pyplot as plt
 
-from ._distmat import sort_dist_mat
 
-
-def plot_dist_mat(dists, keys, title=None, fig_size=None, sort=False):
-    if sort:
-        dists, sorted_idx = sort_dist_mat(dists)
-        keys = np.asarray(keys)[sorted_idx]
-
+def plot_dist_mat(dists, title=None, fig_size=None):
+    # TODO: add as_method to PairwiseDists
     fig, ax = plt.subplots(figsize=fig_size)
 
-    im = ax.imshow(dists)
+    im = ax.imshow(dists.matrix)
 
     plt.colorbar(im)
 
     if title is not None:
         ax.set_title(title)
 
+    keys = dists.labels
     ax.set_xticks(range(len(keys)))
     ax.set_xticklabels(keys, rotation=90)
 
