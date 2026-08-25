33525201fc34a445d3764ba8bce71d5b8316687d
Luís F. Pereira <luisfgper@gmail.com>
2026-08-24 20:30:49 -0700

    Add center to BCVBlock

--- polpo/bcv/block.py
+++ polpo/bcv/block.py
@@ -1,25 +1,27 @@
 import numpy as np
 
-# TODO: check need to remove mean
-
 
 def full_svd(X):
     return np.linalg.svd(X, full_matrices=False)
 
 
 class BCVBlock:
-    def __init__(self, heldout_rows, heldout_cols, svd=full_svd):
+    def __init__(self, heldout_rows, heldout_cols, center=False, svd=full_svd):
         self.heldout_rows = heldout_rows
         self.heldout_cols = heldout_cols
+        self.center = center
         self.svd = svd
 
-    def _partition(self, X):
+    def _masks(self, X):
         row_mask = np.zeros(X.shape[0], dtype=bool)
         col_mask = np.zeros(X.shape[1], dtype=bool)
 
         row_mask[self.heldout_rows] = True
         col_mask[self.heldout_cols] = True
 
+        return row_mask, col_mask
+
+    def _partition(self, X, row_mask, col_mask):
         return (
             X[np.ix_(row_mask, col_mask)],
             X[np.ix_(row_mask, ~col_mask)],
@@ -28,14 +30,37 @@ class BCVBlock:
         )
 
     def fit(self, X):
-        self.A_, self.B_, self.C_, self.D_ = self._partition(X)
+        row_mask, col_mask = self._masks(X)
+
+        if self.center:
+            self.mean_ = X[~row_mask].mean(axis=0)
+            X = X - self.mean_
+        else:
+            self.mean_ = None
+
+        self.A_, self.B_, self.C_, self.D_ = self._partition(X, row_mask, col_mask)
 
         self.U_, self.s_, self.Vt_ = self.svd(self.D_)
 
         return self
 
-    def predict(self, rank):
+    def _resolve_rank(self, rank):
+        n_singular_values = len(self.s_)
+
+        if rank is None:
+            return n_singular_values
+
+        if not 1 <= rank <= n_singular_values:
+            raise ValueError(
+                f"rank must be between 1 and {n_singular_values},got {rank}."
+            )
+
+        return rank
+
+    def predict(self, rank=None):
         # A_k=B D_k^{+} C
+
+        rank = self._resolve_rank(rank)
 
         U = self.U_[:, :rank]
         s = self.s_[:rank]
@@ -44,11 +69,14 @@ class BCVBlock:
         # if constructing full matrix
         # D_pinv = V @ np.diag(1 / s) @ U.T
 
-        # TODO: need to make it more stable to avoid ZeroDivisionError?
-        return (self.B_ @ V / s) @ (U.T @ self.C_)
+        s_inv = np.where(s > 1e-12, 1.0 / s, 0.0)
+        return (self.B_ @ V * s_inv) @ (U.T @ self.C_)
 
-    def approximation(self, rank):
+    def approximation(self, rank=None):
         # $D_k=U_k \Sigma_k V_k^{\top}$
+
+        rank = self._resolve_rank(rank)
+
         U = self.U_[:, :rank]
         s = self.s_[:rank]
         Vt = self.Vt_[:rank]
@@ -67,29 +95,37 @@ class BCVBlock:
 
         return error
 
-    def _resolve_max_rank(self, max_rank):
-        n_singular_values = len(self.s_)
+    def predictions(self, max_rank=None):
+        # exploits \hat{A}_k=\sum_{j=1}^k \frac{\left(B v_j\right)\left(u_j^{\top} C\right)}{\sigma_j}
 
-        if max_rank is None:
-            return n_singular_values
+        max_rank = self._resolve_rank(max_rank)
 
-        if not 1 <= max_rank <= n_singular_values:
-            raise ValueError(
-                f"max_rank must be between 1 and {n_singular_values}, "
-                f"got {max_rank}."
-            )
+        U = self.U_[:, :max_rank]
+        s = self.s_[:max_rank]
+        V = self.Vt_[:max_rank].T
 
-        return max_rank
+        BV = self.B_ @ V
+        UTC = U.T @ self.C_
+
+        BV = np.divide(
+            BV,
+            s,
+            out=np.zeros_like(BV),
+            where=s > 1e-12,
+        )
+
+        components = BV.T[:, :, None] * UTC[:, None, :]
+        return np.cumsum(components, axis=0)
 
     def residuals(self, max_rank=None):
-        max_rank = self._resolve_max_rank(max_rank)
-
-        return np.stack([self.residual(rank) for rank in range(1, max_rank + 1)])
+        return self.A_[None, :, :] - self.predictions(max_rank)
 
     def errors(self, max_rank=None, normalize=False):
-        max_rank = self._resolve_max_rank(max_rank)
-        # TODO: vectorize?
+        residuals = self.residuals(max_rank)
 
-        return np.array(
-            [self.error(rank, normalize=normalize) for rank in range(1, max_rank + 1)]
-        )
+        errors = np.linalg.norm(residuals, ord="fro", axis=(1, 2)) ** 2
+
+        if normalize:
+            errors /= self.A_.size
+
+        return errors
