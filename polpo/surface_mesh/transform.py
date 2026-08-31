a887cc0365363bd1beab8ed00bbf5096242b1223
Luís F. Pereira <luisfgper@gmail.com>
2026-08-31 16:18:48 -0700

    Add initial implementation of Transform

--- polpo/surface_mesh/transform.py
+++ polpo/surface_mesh/transform.py
@@ -48,3 +48,113 @@ class PvSurfaceToVertices:
     def inverse_tangent(self, image_tangent_vec, image_point=None, base_point=None):
         # TODO: need to check vectorization
         return gs.asarray(image_tangent_vec)
+
+
+class InvertibleTransform:
+    def transform(self, x):
+        raise NotImplementedError
+
+    def inverse_transform(self, x):
+        raise NotImplementedError
+
+
+class DeltaTransform(InvertibleTransform):
+    """Transform meshes to and from flattened template-relative deltas.
+
+    Parameters
+    ----------
+    template : PvSurface
+        Reference mesh defining the common vertex topology and origin for the
+        displacement representation.
+
+    Notes
+    -----
+    The forward transform maps each mesh to the flattened vertex displacement
+
+        mesh.vertices - template.vertices
+
+    with shape ``(..., 3 * n_vertices)``.
+
+    The inverse transform reshapes these displacements to vertex coordinates,
+    adds them to the template vertices, and returns meshes with the template
+    topology.
+    """
+
+    def __init__(self, template):
+        self.template = template
+        self.n_vertices = len(template.vertices)
+
+    def transform(self, meshes):
+        """Transform meshes into flattened vertex deltas.
+
+        Parameters
+        ----------
+        meshes : PvSurface or array-like of PvSurface, shape [...]
+            Mesh or collection of meshes sharing the template topology.
+
+        Returns
+        -------
+        deltas : array-like, shape [..., 3 * n_vertices]
+            Flattened vertex displacements relative to the template.
+        """
+        meshes = gs.asarray(meshes, dtype=object)
+        batch_shape = meshes.shape
+
+        deltas = [
+            gs.reshape(mesh.vertices - self.template.vertices, (-1,))
+            for mesh in meshes.flat
+        ]
+
+        return gs.reshape(
+            gs.stack(deltas),
+            batch_shape + (3 * self.n_vertices,),
+        )
+
+    def inverse_transform(self, deltas):
+        """Transform flattened vertex deltas back into meshes.
+
+        Parameters
+        ----------
+        deltas : array-like, shape [..., 3 * n_vertices]
+            Flattened vertex displacements relative to the template.
+
+        Returns
+        -------
+        meshes : PvSurface or ndarray of PvSurface, shape [...]
+            Reconstructed mesh or collection of meshes with the template
+            topology.
+        """
+        single = deltas.ndim == 1
+
+        if single:
+            deltas = deltas[None, ...]
+
+        batch_shape = deltas.shape[:-1]
+        deltas = gs.reshape(deltas, (-1, self.n_vertices, 3))
+
+        meshes = gs.empty(len(deltas), dtype=object)
+
+        for i, delta in enumerate(deltas):
+            pv_mesh = self.template.as_pv().copy(deep=True)
+            pv_mesh.points = gs.to_numpy(self.template.vertices + delta)
+            meshes[i] = PvSurface(pv_mesh)
+
+        if single:
+            return meshes[0]
+
+        return meshes.reshape(batch_shape)
+
+
+class CompositeTransform(InvertibleTransform):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def transform(self, x):
+        for transform in self.transforms:
+            x = transform.transform(x)
+        return x
+
+    def inverse_transform(self, x):
+        for transform in reversed(self.transforms):
+            x = transform.inverse_transform(x)
+        return x
